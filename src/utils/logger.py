ee93c100200a75f82c7ba7e719e01a6dd75e06bb
kitagrasa <261068202+kitagrasa@users.noreply.github.com>
2026-02-12 22:54:38 +0100

    Update logger.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,10 +1,9 @@
 import logging
 import sys
-from datetime import datetime
 
 def setup_logger(name):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)  # Cambiar a INFO para logs limpios
+    logger.setLevel(logging.INFO)
     
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(logging.INFO)
